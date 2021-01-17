2fe58083e1d2607e9ef18a9e656b9f11ca36983d
Naylin88 <naylinoo2009@gmail.com>
2021-01-17 13:17:43 +0630

    Coa and Coa Type Model

---
+++ Models/Coa.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InitCMS.Models
+{
+    public class Coa
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(16)]
+        public string Code { get; set; }
+        [MaxLength(30)]
+        public string Description { get; set; }
+        public int CoaTypeId { get; set; }
+        public CoaType CoaType { get; set; }
+    }
+}
---
+++ Models/CoaType.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace InitCMS.Models
+{
+    public class CoaType
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(3)]
+        public string Label { get; set; }
+        [MaxLength(10)]
+        public string Description { get; set; }
+        public ICollection<Coa> Coas { get; set; }
+    }
+}
